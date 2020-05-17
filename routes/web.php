14667e653e6eafec671ab0920ba7f5c16348cf14
yuichiro-cloud <ghjhbc1@gmail.com>
2020-05-17 15:50:54 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth']], function(){
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+});
